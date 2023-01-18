3e4890eabb315db2e44d2ae4c3866fa2449a9808
commentline <gitcommentline@gmail.com>
2023-01-18 19:43:15 +0300

    Add thunk middleware to store structure

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
-import { createStore } from "redux"
-import rootReducer from "./rootReducer"
-import { devToolsEnhancer } from "redux-devtools-extension"
+import { applyMiddleware, compose, createStore } from "redux";
+import rootReducer from "./rootReducer";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const configureStore = () => {
-  return createStore(rootReducer, devToolsEnhancer())
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 }
